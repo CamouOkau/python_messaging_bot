9139f2c2dc38750e3f0f04ec6b94e771b22ce95d
Feetless <endendupaparmaliu123@gmail.com>
2022-01-01 01:41:42 -0500

    Updated the code.

--- code/bot.py
+++ code/bot.py
@@ -2,30 +2,24 @@ from fbchat import Client
 from fbchat.models import Message as fb_message
 
 
-class MessengerBot:
+class FbMessengerBot:
     def __init__(self, email, password):
-        """ A bot for sending messages through messenger. """
         self.email = email
         self.password = password
         self.client = Client(email, password)
 
-    def uid(self, username):
-        """ Returns the uid of the user. """
+    def get_uid(self, username):
         try:
             user = self.client.searchForUsers(username)[0]
             return user.uid
-
         except IndexError:
             print(f"User \"{username}\" does not exist!")
-    
+
     def send_message(self, message, uid):
-        """ Send a message to uid. """
-        if not type(uid) is None:
+        if type(uid) is None:
+            print(f"Uid \"{uid}\" does not exist!")
+        else:
             self.client.send(fb_message(message), uid)
 
-        else:
-            print(f"Uid \"{uid}\" does not exist!")
-    
     def logout(self):
-        """ logs out with the bot. """
         self.client.logout()
--- code/send.py
+++ code/send.py
@@ -1,37 +1,27 @@
 import sys
 import time
 from datetime import datetime
-from bot import MessengerBot
+from bot import FbMessengerBot
 
 
 if __name__ == "__main__":
-    try:
-        email = sys.argv[1]
-        password = sys.argv[2]
-
-        bot = MessengerBot(email, password)
-
+    if len(sys.argv) < 3:
+        print("No email or password provided")
+    else:
+        bot = FbMessengerBot(sys.argv[1], sys.argv[2])
         with open("users.txt", "r") as file:
             users = dict.fromkeys(file.read().split("\n"))
-
             for user in users:
                 users[user] = bot.uid(user)
-
         with open("message.txt", "r") as file:
             message = file.read()
-        
+
         time_now = datetime.now()
         send_time = datetime(time_now.year + 1, 1, 1)
-
         wait_time = (send_time - time_now).total_seconds()
-        
         print("Waiting...")
         time.sleep(wait_time)
 
         for uid in users.values():
             bot.send_message(message, uid)
-
         bot.logout()
-
-    except IndexError:
-            print("No email or password")
